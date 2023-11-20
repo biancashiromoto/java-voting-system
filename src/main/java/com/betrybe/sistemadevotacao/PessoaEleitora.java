4057f64b6148e4512bb919fd502c282a111b8d6c
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-11-20 16:32:54 -0300

    feat: Implement class PessoaEleitora

--- src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaEleitora.java
@@ -4,5 +4,25 @@ package com.betrybe.sistemadevotacao;
  * PessoaEleitora.
  */
 public class PessoaEleitora extends Pessoa {
+  private String cpf;
 
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+
+  /**
+   * {@summary Elector.}
+   *
+   * @param nome Elector's name
+   *
+   * @param cpf Elector's cpf
+   */
+  public PessoaEleitora(String nome, String cpf) {
+    this.nome = nome;
+    this.cpf = cpf;
+  }
 }
