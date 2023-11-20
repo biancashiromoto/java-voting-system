2861a0434705714cecf378815cbc2ecd4db3fb7c
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-11-20 15:40:05 -0300

    feat: Implement class PessoaCandidata

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -3,6 +3,30 @@ package com.betrybe.sistemadevotacao;
 /**
  * Pessoa candidata.
  */
-public class PessoaCandidata {
+public class PessoaCandidata extends Pessoa {
+  int numero = 0;
 
+  public int getNumero() {
+    return numero;
+  }
+
+  public int getVotos() {
+    return votos;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  int votos = 0;
+
+  public PessoaCandidata(String nome, int numero) {
+    this.numero = numero;
+    this.votos = 0;
+    this.nome = nome;
+  }
+
+  public void receberVoto() {
+    votos += 1;
+  }
 }
