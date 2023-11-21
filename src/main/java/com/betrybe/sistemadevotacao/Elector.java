38411b8a71c4d8c737cba3e39433ff6733fe0347
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-11-21 19:09:18 -0300

    chore: Add Javadoc comments

--- src/main/java/com/betrybe/sistemadevotacao/Elector.java
+++ src/main/java/com/betrybe/sistemadevotacao/Elector.java
@@ -1,25 +1,43 @@
 package com.betrybe.sistemadevotacao;
 
 /**
- * PessoaEleitora.
+ * Represents an elector in the voting system.
+ * This class extends the Person class and includes additional information:
+ * - Person's CPF.
+ *
+ * @see Person
  */
 public class Elector extends Person {
+
+  /**
+   * Elector's CPF.
+   * Will be reasigned a value when registering a new elector.
+   */
   private String cpf;
 
+  /**
+   * Elector's CPF attribute Getter.
+   *
+   * @return The elector's CPF.
+   */
   public String getCpf() {
     return cpf;
   }
 
+  /**
+   * Elector's CPF attribute Setter.
+   *
+   * @param cpf The elector's CPF.
+   */
   public void setCpf(String cpf) {
     this.cpf = cpf;
   }
 
   /**
-   * {@summary Elector.}
+   * The Elector's class constructor.
    *
-   * @param name Elector's name
-   *
-   * @param cpf Elector's cpf
+   * @param name The elector's name.
+   * @param cpf The elector's CPF.
    */
   public Elector(String name, String cpf) {
     this.name = name;
