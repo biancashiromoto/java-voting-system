02e1e21c709865a07c6d30401d52de287058bb42
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-11-21 19:00:33 -0300

    chore: Add Javadoc comments

--- src/main/java/com/betrybe/sistemadevotacao/Person.java
+++ src/main/java/com/betrybe/sistemadevotacao/Person.java
@@ -1,20 +1,34 @@
 package com.betrybe.sistemadevotacao;
 
 /**
- * Person.
+ * Represents a person in the voting system.
+ * Serves as the base class for specific person types.
+ *
+ * @see Candidate
+ * @see Elector
  */
 abstract class Person {
+
+  /**
+   * The person's name.
+   */
   protected String name;
 
+  /**
+   * The person's name attribute Getter.
+   *
+   * @return The person's name.
+   */
   public String getName() {
     return name;
   }
 
+  /**
+   * The person's name attribute Setter.
+   *
+   * @param name The person's name.
+   */
   public void setName(String name) {
     this.name = name;
-  }
-
-  public static void main(String[] args) {
-
   }
 }
